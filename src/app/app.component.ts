ae4ebc367a0b40e2aec6b4299db48406077858ea
Sarmiran <m_sarmiran@hotmail.com>
2019-05-06 16:09:58 -0400

    prepare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'TODOAPPREAL';
+  todoArray = [];
+  todo;
+  marked = false;
+  theCheckbox = false;
+
+  addTodo(value) {
+    this.todoArray.push(value);
+    console.log(this.todo);
+  }
+  /*delete item*/
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
+  // submit Form
+  todoSubmit(value: any) {
+    if (value !== "") {
+      this.todoArray.push(value.todo);
+      //this.todoForm.reset()
+    } else {
+      alert("Field required **");
+    }
+  }
+  toggleVisibility(e) {
+    this.marked = e.target.checked;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { TodolistComponent } from "./todolist/todolist.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodolistComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
